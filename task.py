b5438f9d3a263f8c3f38b2c23523363e623a1da8
maks-mishin <maks.mishinFZ@gmail.com>
2022-08-21 18:32:00 +0300

    solve task 2

--- task.py
+++ task.py
@@ -1,8 +1,13 @@
-def squirell( N ):
-    factorial = 1
-    for i in range(N):
-        factorial *= i + 1
-    
-    while factorial > 10:
-        factorial = factorial // 10
-    return factorial
+def odometer( oksana ):
+    elapsed_time = 0
+    distance = 0
+    current_velocity = 0
+
+    for i in range(len(oksana)):
+        if i % 2 == 0:
+            current_velocity = oksana[i]
+        else:
+            delta_time = oksana[i] - elapsed_time
+            elapsed_time = oksana[i]
+            distance += current_velocity * delta_time
+    return distance
