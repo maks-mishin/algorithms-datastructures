ccc6a131cef452be40657c92cd40124d57011aaf
maks-mishin <maks.mishinFZ@gmail.com>
2022-08-22 22:23:42 +0300

    solve task 3

--- task.py
+++ task.py
@@ -1,13 +1,53 @@
-def odometer( oksana ):
-    elapsed_time = 0
-    distance = 0
-    current_velocity = 0
+def ConquestCampaign( N, M, L, battalion ):
+    place_of_arms = []
 
-    for i in range(len(oksana)):
-        if i % 2 == 0:
-            current_velocity = oksana[i]
-        else:
-            delta_time = oksana[i] - elapsed_time
-            elapsed_time = oksana[i]
-            distance += current_velocity * delta_time
-    return distance
+    for i in range(N):
+        tmp_list = []
+        for j in range(M):
+            tmp_list.append(False)
+        place_of_arms.append(tmp_list)
+
+    places_already_control = set()
+
+    for i in range(0, len(battalion) - 1, 2):
+        y_coord = battalion[i] - 1
+        x_coord = battalion[i + 1] - 1
+        place_of_arms[y_coord][x_coord] = True
+
+        places_already_control.add((y_coord, x_coord))
+
+    day_total_control = 1
+
+    neighbours = [1, -1]
+    while True:
+        day_total_control += 1
+
+        flag = True
+        temp_places_already_control = []
+
+        for place in places_already_control:
+            i = place[0]
+            j = place[1]
+
+            if place_of_arms[i][j] == True:
+                for neighbour in neighbours:
+                    cell_vertical = i + neighbour
+                    if 0 <= cell_vertical <= N - 1:
+                        place_of_arms[cell_vertical][j] = True
+                        temp_places_already_control.append((cell_vertical, j))
+
+                    cell_horizontal = j + neighbour
+                    if 0 <= cell_horizontal <= M - 1:
+                        place_of_arms[i][cell_horizontal] = True
+                        temp_places_already_control.append((i, cell_horizontal))
+
+        for place in temp_places_already_control:
+            places_already_control.add(place)
+
+        for i in range(N):
+            for j in range(M):
+                flag = flag and place_of_arms[i][j]
+
+        if flag:
+            break
+    return day_total_control
