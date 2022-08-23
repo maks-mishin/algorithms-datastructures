4da6b3d4c2cdf9bc4c8e5396cb0a2975668ffbd0
maks-mishin <maks.mishinFZ@gmail.com>
2022-08-23 21:19:03 +0300

    add PatternUnlock solution

--- task.py
+++ task.py
@@ -1,11 +1,19 @@
-def SynchronizingTables( N, ids, salary ):
-    sorted_ids, sorted_salary = sorted(ids), sorted(salary)
+def PatternUnlock( N, hits ):
+    keyboard_coords = {
+        6: (1, 1), 1: (1, 2), 9: (1, 3),
+        5: (2, 1), 2: (2, 2), 8: (2, 3),
+        4: (3, 1), 3: (3, 2), 7: (3, 3)
+    }
     
-    correct_map_id_salary = {}
-    for i in range(N):
-        correct_map_id_salary[sorted_ids[i]] = sorted_salary[i]
-
-    correct_salary = []
-    for id_worker in ids:
-        correct_salary.append(correct_map_id_salary[id_worker])
-    return correct_salary
+    total_lenght_line = 0
+    for i in range(N - 1):
+        x_dist = abs(keyboard_coords[hits[i]][0] - keyboard_coords[hits[i + 1]][0])
+        y_dist = abs(keyboard_coords[hits[i]][1] - keyboard_coords[hits[i + 1]][1])
+        if x_dist == 1 and y_dist == 1:
+            total_lenght_line += 2 ** (1/2)
+        else:
+            total_lenght_line += 1
+    
+    total_lenght_line = round(total_lenght_line, 5)
+    result_str = str(total_lenght_line).replace('0', '').replace('.', '')
+    return result_str
