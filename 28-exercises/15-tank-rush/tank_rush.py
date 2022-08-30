07424cab2685c50a0aafd4412a34c17e9846f76e
Maks Mishin <maks.mishinFZ@gmail.com>
2022-08-30 12:27:08 +0300

    change 15-TankRush solution

--- 28-exercises/15-tank-rush/tank_rush.py
+++ 28-exercises/15-tank-rush/tank_rush.py
@@ -1,3 +1,15 @@
+def is_total_matching(tanks_map: list, place_map: list, i: int, j: int) -> bool:
+    tanks_map_rows, tanks_map_cols = len(tanks_map), len(tanks_map[0])
+    
+    is_match_position = True
+    for row in range(tanks_map_rows):
+        is_match_position = (
+            is_match_position and
+            tanks_map[row] == place_map[i + row][j:tanks_map_cols + j]
+        )
+    return is_match_position
+
+
 def TankRush(rows1: int, cols1: int, S1: str,
              rows2: int, cols2: int, S2: str) -> bool:
     if rows2 > rows1 or cols2 > cols1:
@@ -5,21 +17,12 @@ def TankRush(rows1: int, cols1: int, S1: str,
     if rows2 * cols2 == 0:
         return True
 
-    overall_map, searched_map = S1.split(), S2.split()
-    start_row = 0
+    place_map, tanks_map = S1.split(), S2.split()
     for i in range(rows1 - rows2 + 1):
-        is_included = True
-        for j in range(rows2):
-            is_included = is_included and searched_map[j] in overall_map[i + j]
-        if is_included:
-            start_row = i
-            break
-
-    index_tank = overall_map[start_row].find(searched_map[0])
-    for i in range(start_row, rows1 - rows2 + 1):
-        for j in range(1, rows2):
-            matching_index = (
-                index_tank == overall_map[i + j].find(searched_map[j])
-            )
-            is_included = is_included and matching_index
-    return is_included
+        for j in range(cols1 - cols2 + 1):
+            if (
+                place_map[i][j] == tanks_map[0][0] and
+                is_total_matching(tanks_map, place_map, i, j)
+            ):
+                return True
+    return False
