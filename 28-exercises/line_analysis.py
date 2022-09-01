73cf19c3c284191df038ef9a3f7f54076d59aa9d
Maks Mishin <maks.mishinFZ@gmail.com>
2022-09-01 12:38:55 +0300

    add 17-LineAnalysis solution

---
+++ 28-exercises/line_analysis.py
@@ -0,0 +1,24 @@
+def is_template_line(line: str) -> bool:
+    template = ''
+    for ch in line[1:]:
+        if ch == '.':
+            template += ch
+        if ch == '*':
+            template += ch
+            break
+    # construct the source line fromby template
+    constructed_line = '*'
+    while len(constructed_line) < len(line):
+        constructed_line += template
+    return constructed_line == line
+
+
+def LineAnalysis(line: str) -> bool:
+    # check the source line contains only '*' and '.'
+    is_correct_line = all(map(lambda x: x == '*' or x == '.', line))
+    if not is_correct_line:
+        return False
+    
+    if not line[0] == line[-1] == '*':
+        return False
+    return is_template_line(line)
