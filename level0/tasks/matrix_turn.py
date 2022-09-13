8c384e70f4ce3546d5e874d9869eab265e7d760e
Maks Mishin <maks.mishinFZ@gmail.com>
2022-09-13 12:45:04 +0300

    feat: add 24-MatrixTurn solution

---
+++ level0/tasks/matrix_turn.py
@@ -0,0 +1,40 @@
+def rotate_one_step(matrix: list, rows: int, cols: int) -> None:
+    top, bottom = 0, rows - 1
+    left, right = 0, cols - 1
+    
+    count_layers_rotated = int(rows / 2)
+    for _ in range(count_layers_rotated):
+        top_left = matrix[left][left]
+        
+        for i in range(top, bottom):
+            matrix[i][left] = matrix[i + 1][left]
+        
+        for j in range(left, right):
+            matrix[bottom][j] = matrix[bottom][j + 1]
+        
+        for i in range(bottom - 1, top - 1, -1):
+            matrix[i + 1][right] = matrix[i][right]
+            
+        for j in range(right - 1, left, -1):
+            matrix[top][j + 1] = matrix[top][j]
+        
+        matrix[left][left + 1] = top_left
+        
+        if left == cols - 1 or top == rows - 1:
+            break
+        top, bottom = top + 1, bottom - 1
+        left, right = left + 1, right - 1
+
+
+def MatrixTurn(matrix: list, rows: int, cols: int, steps: int) -> None:
+    # convert each row to list because of
+    # 'str' object does not suppost item assignment
+    for i, row in enumerate(matrix):
+        matrix[i] = list(row)
+    
+    for _ in range(steps):
+        rotate_one_step(matrix, rows, cols)
+    
+    # convert back to str
+    for i, row in enumerate(matrix):
+        matrix[i] = ''.join(row)
