0c896d1af6a9277f8318005d534844ddf4041bbb
Maks Mishin <maks.mishinFZ@gmail.com>
2022-09-15 12:12:54 +0300

    feat: add 27-Football solution

---
+++ level0/tasks/football.py
@@ -0,0 +1,31 @@
+def swap_players(players: list, count_players: int) -> tuple:
+    copy_players = players
+    copy_players[i], copy_players[j] = copy_players[j], copy_players[i]
+    return tuple(copy_players)
+
+
+def can_sorting_swap(players: list, count_players: int) -> bool:
+    player_permutations = set()
+    for i in range(count_players):
+        for j in range(count_players):
+            player_permutations.add(swap_players(players, i, j))
+    return tuple(sorted(players)) in player_permutations
+
+
+def can_sorting_reverse(players: list, count_players: int) -> bool:
+    new_players_positions = set()
+    for i in range(count_players - 1):
+        for j in range(2, count_players):
+            new_players_positions.add(
+                tuple(
+                    players[:i] +
+                    list(reversed(players[i:i + j])) +
+                    players[i + j:]
+                )
+            )
+    return tuple(sorted(players)) in new_players_positions
+
+
+def Football(players: list, count_players: int) -> bool:
+    return (can_sorting_reverse(players, count_players)
+            or can_sorting_swap(players, count_players))
