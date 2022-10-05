516b213cfb4b45db68cf303ba7c1034e5506ab84
maks-mishin <maks.mishinFZ@gmail.com>
2022-10-05 23:21:44 +0300

    feat: add Dynamic Array implementation

---
+++ level1/tasks/dynamic_array.py
@@ -0,0 +1,70 @@
+import ctypes
+
+
+class DynArray:
+    """Dynamic array class (similar to Python list)"""
+
+    def __init__(self):
+        self.count = 0
+        self.capacity = 16
+        self.min_capacity = 16
+        self.array = self.make_array(self.capacity)
+
+    def make_array(self, new_capacity):
+        return (new_capacity * ctypes.py_object)()
+
+    def __len__(self):
+        return self.count
+
+    def __getitem__(self, i):
+        if i < 0 or i >= self.count:
+            raise IndexError('Index is out of bounds')
+        return self.array[i]
+
+    def resize(self, new_capacity):
+        new_array = self.make_array(new_capacity)
+        for i in range(self.count):
+            new_array[i] = self.array[i]
+        self.array = new_array
+        self.capacity = new_capacity
+
+    def append(self, item):
+        if self.count == self.capacity:
+            self.resize(2 * self.capacity)
+        self.array[self.count] = item
+        self.count += 1
+
+    def insert(self, i, item):
+        if i < 0 or i > self.count:
+            raise IndexError('Index is out of bounds')
+        if i == self.count:
+            self.append(item)
+            return
+        self._increase_capacity()
+
+        for pos in range(self.count - 1, i - 1, -1):
+            self.array[pos + 1] = self.array[pos]
+        self.array[i] = item
+        self.count += 1
+
+    def _increase_capacity(self):
+        if self.count + 1 > self.capacity:
+            self.resize(2 * self.capacity)
+
+    def print(self):
+        for i in range(self.count):
+            print(self.array[i], end=' ')
+
+    def _shrink_capacity(self):
+        if (self.count / self.capacity < 0.5 and
+                int(self.capacity / 1.5) >= self.min_capacity):
+            self.capacity = int(self.capacity / 1.5)
+
+    def delete(self, i):
+        if i < 0 or i >= self.count:
+            raise IndexError('Index is out of bounds')
+        for pos in range(i, self.count - 1):
+            self.array[pos] = self.array[pos + 1]
+        self.array[self.count - 1] = None
+        self.count -= 1
+        self._shrink_capacity()
---
+++ level1/test_dynamic_array.py
@@ -0,0 +1,113 @@
+import unittest
+
+from tasks.dynamic_array import DynArray
+
+
+class TestDynamicArray(unittest.TestCase):
+    def test_constructor(self):
+        da = DynArray()
+        self.assertEqual(da.count, 0)
+        self.assertEqual(da.min_capacity, 16)
+        self.assertEqual(da.capacity, 16)
+
+    def test_resize(self):
+        da = DynArray()
+        self.assertEqual(da.capacity, 16)
+        da.resize(89)
+        self.assertEqual(da.capacity, 89)
+
+    def test_get_item(self):
+        da = DynArray()
+        try:
+            item = da[0]
+        except IndexError as ex:
+            self.assertEqual(str(ex), 'Index is out of bounds')
+
+        da.append(1)
+        self.assertEqual(da[0], 1)
+
+        try:
+            item = da[-1]
+        except IndexError as ex:
+            self.assertEqual(str(ex), 'Index is out of bounds')
+
+        try:
+            item = da[len(da) + 1]
+        except IndexError as ex:
+            self.assertEqual(str(ex), 'Index is out of bounds')
+
+    def test_len(self):
+        da = DynArray()
+        self.assertEqual(len(da), 0)
+        da.append(1)
+        self.assertEqual(len(da), 1)
+
+    def test_append(self):
+        da = DynArray()
+        da.append(9)
+        self.assertEqual(da[0], 9)
+        self.assertEqual(len(da), 1)
+
+    def test_insert_not_full_buffer(self):
+        da = DynArray()
+        inserted_position = 0
+        inserted_value = 5
+        da.insert(inserted_position, inserted_value)
+
+        self.assertEqual(da.capacity, 16)
+        self.assertEqual(da.count, 1)
+        self.assertEqual(len(da), 1)
+        self.assertEqual(da[inserted_position], inserted_value)
+
+    def test_insert_overflow_buffer(self):
+        da = DynArray()
+        for i in range(16):
+            da.append(i)
+        self.assertEqual(da.capacity, 16)
+        da.insert(0, 90)
+        self.assertEqual(da[0], 90)
+        self.assertEqual(da.capacity, 32)
+
+    def test_insert_incorrect_position(self):
+        da = DynArray()
+        try:
+            da.insert(9, 18)
+        except IndexError as ex:
+            self.assertEqual(
+                str(ex), 'Index is out of bounds'
+            )
+
+    def test_delete_prev_buffer(self):
+        da = DynArray()
+        da.append(1)
+        da.append(9)
+        self.assertEqual(da.capacity, 16)
+        self.assertEqual(len(da), 2)
+        da.delete(0)
+        self.assertEqual(da.capacity, 16)
+        self.assertEqual(len(da), 1)
+
+    def test_delete_reduce_buffer(self):
+        da = DynArray()
+        self.assertEqual(da.capacity, 16)
+        for i in range(17):
+            da.append(i)
+        self.assertEqual(len(da), 17)
+        self.assertEqual(da.capacity, 32)
+        da.delete(0)
+        self.assertEqual(len(da), 16)
+        self.assertEqual(da.capacity, 32)
+        da.delete(0)
+        self.assertEqual(len(da), 15)
+        self.assertEqual(da.capacity, 21)
+
+    def test_delete_incorrect_position(self):
+        da = DynArray()
+        try:
+            da.delete(-1)
+        except IndexError as ex:
+            self.assertEqual(str(ex), 'Index is out of bounds')
+
+
+if __name__ == '__main__':
+    unittest.main()
