b4db07a1e9cade18f8104ecf503535099c720ce8
maks-mishin <maks.mishinFZ@gmail.com>
2022-10-08 17:54:14 +0300

    refactor: simplify postfix operations

--- level1/tasks/postfix_expression.py
+++ level1/tasks/postfix_expression.py
@@ -1,21 +1,25 @@
 from stack import Stack
 
 
+postfix_operations = {
+    '+': lambda lhs, rhs: lhs + rhs,
+    '-': lambda lhs, rhs: lhs - rhs,
+    '*': lambda lhs, rhs: lhs * rhs,
+    '/': lambda lhs, rhs: lhs / rhs,
+}
+
+
 def calc_postfix_expression(expression: str):
     stack1, stack2 = Stack(), Stack()
     for symbol in reversed(expression.split()):
         stack1.push(symbol)
+
     while stack1.size() > 0:
         if stack1.peek().isdigit():
             stack2.push(int(stack1.pop()))
             continue
-        if stack1.peek() == '+':
-            stack2.push(
-                stack2.pop() + stack2.pop()
-            )
-        if stack1.peek() == '*':
-            stack2.push(
-                stack2.pop() * stack2.pop()
-            )
+        stack2.push(
+            postfix_operations[stack1.peek()](stack2.pop(), stack2.pop())
+        )
         stack1.pop()
     return stack2.pop()
