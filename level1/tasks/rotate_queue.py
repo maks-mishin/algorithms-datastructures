705d03e4c7c845cba21ee81d51d6519a9afe90c9
maks-mishin <maks.mishinFZ@gmail.com>
2022-10-09 16:27:36 +0300

    fix: rotation queue by N steps

--- level1/tasks/rotate_queue.py
+++ level1/tasks/rotate_queue.py
@@ -7,5 +7,7 @@ def cyclic_shift_queue(queue: Queue):
 
 
 def rotate_queue(queue: Queue, number_shifts: int):
+    if queue.size() == 0:
+        return
     for _ in range(number_shifts):
         cyclic_shift_queue(queue)
