0c50a6a14a8ce5796917d8965b62057556adfb69
maks-mishin <maks.mishinFZ@gmail.com>
2022-10-09 17:50:32 +0300

    fix: fixed tests for Deque

--- level1/test_deque.py
+++ level1/test_deque.py
@@ -12,26 +12,48 @@ def check_elem_in_deque(deq: Deque, val) -> bool:
     return False
 
 
+def make_list_from_deq(deq: Deque) -> list:
+    result_list = []
+    node = deq.head.next
+    while isinstance(node, Node):
+        result_list.append(node.value)
+        node = node.next
+    result_list.pop()
+    return result_list
+
+
 class TestDeque(unittest.TestCase):
     def test_add_front(self):
         deq = Deque()
         deq.addFront(15)
         self.assertEqual(deq.size(), 1)
         self.assertTrue(check_elem_in_deque(deq, 15))
+        self.assertEqual(
+            make_list_from_deq(deq), [15]
+        )
 
         deq.addFront(17)
         self.assertEqual(deq.size(), 2)
         self.assertTrue(check_elem_in_deque(deq, 17))
+        self.assertEqual(
+            make_list_from_deq(deq), [17, 15]
+        )
 
     def test_add_tail(self):
         deq = Deque()
         deq.addTail(15)
         self.assertEqual(deq.size(), 1)
         self.assertTrue(check_elem_in_deque(deq, 15))
+        self.assertEqual(
+            make_list_from_deq(deq), [15]
+        )
 
         deq.addTail(17)
         self.assertEqual(deq.size(), 2)
         self.assertTrue(check_elem_in_deque(deq, 17))
+        self.assertEqual(
+            make_list_from_deq(deq), [15, 17]
+        )
 
     def test_remove_front(self):
         deq = Deque()
