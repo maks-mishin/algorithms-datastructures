61284d2329d33b82556acfbf5976256a2c67df82
maks-mishin <maks.mishinFZ@gmail.com>
2022-11-07 22:28:29 +0300

    feat: add solution for second variable names task

--- survivor/tasks/synchronizing_tables.py
+++ survivor/tasks/synchronizing_tables.py
@@ -1,11 +1,11 @@
-def SynchronizingTables(count: int, ids: list, salary: list) -> list:
-    sorted_ids, sorted_salary = sorted(ids), sorted(salary)
+def SynchronizingTables(count: int, employee_numbers: list, employee_salaries: list) -> list:
+    sorted_ids, sorted_salary = sorted(employee_numbers), sorted(employee_salaries)
 
     correct_map_id_salary = {}
     for i in range(count):
         correct_map_id_salary[sorted_ids[i]] = sorted_salary[i]
 
     correct_salary = []
-    for id_worker in ids:
+    for id_worker in employee_numbers:
         correct_salary.append(correct_map_id_salary[id_worker])
     return correct_salary
--- survivor/tasks/ufo.py
+++ survivor/tasks/ufo.py
@@ -1,12 +1,12 @@
 def UFO(n: int, data: list, octal: bool) -> list:
     base = 8 if octal else 16
 
-    result = []
+    decimal_nums = []
     for num in data:
         current_num = ''.join(reversed(str(num)))
         decimal_num = 0
 
         for power, digit in enumerate(current_num):
             decimal_num += int(digit) * base ** power
-        result.append(decimal_num)
-    return result
+        decimal_nums.append(decimal_num)
+    return decimal_nums
