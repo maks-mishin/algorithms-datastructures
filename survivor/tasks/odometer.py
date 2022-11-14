13e33492b5ac0a86690636ac4fe6331f41405175
maks-mishin <maks.mishinFZ@gmail.com>
2022-11-14 23:49:45 +0300

    feat: add solution for names_to_avoid task

--- survivor/tasks/odometer.py
+++ survivor/tasks/odometer.py
@@ -1,9 +1,9 @@
 def odometer(oksana: list) -> int:
     """Return distance calculated without speedometer"""
-    elapsed_time, distance, current_velocity = 0, 0, 0
+    elapsed_time, distance, velocity = 0, 0, 0
 
     for i in range(0, len(oksana), 2):
-        current_velocity = oksana[i]
+        velocity = oksana[i]
         delta_time, elapsed_time = oksana[i + 1] - elapsed_time, oksana[i + 1]
-        distance += current_velocity * delta_time
+        distance += velocity * delta_time
     return distance
