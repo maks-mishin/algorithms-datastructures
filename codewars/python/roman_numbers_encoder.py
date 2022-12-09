85bbc1e24402744c427868b3bfe939091aa73f0f
maks-mishin <maks.mishinFZ@gmail.com>
2022-12-10 01:52:30 +0300

    feat: add Roman Numerals Encoder solution

---
+++ codewars/python/roman_numbers_encoder.py
@@ -0,0 +1,15 @@
+# Source: https://www.codewars.com/kata/51b62bf6a9c58071c600001b/train/python
+
+def decimal_to_roman(input_num):
+    all_roman = [
+        (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+        (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
+        (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+    ]
+    roman_num = ''
+
+    for decimal, roman in all_roman:
+        while input_num >= decimal:
+            roman_num += roman
+            decimal_num -= decimal
+    return roman_num
