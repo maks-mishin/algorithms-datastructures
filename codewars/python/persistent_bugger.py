eb9a12fed2900e0fd0b7b22d9cbf68a66bfc1363
maks-mishin <maks.mishinFZ@gmail.com>
2022-12-11 11:06:34 +0300

    refactor: soution for Persistent Bugger task

--- codewars/python/persistent_bugger.py
+++ codewars/python/persistent_bugger.py
@@ -1,23 +1,12 @@
 # Source: https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/train/python
 
-import math
-
-
-def make_list_digits_of_num(num):
-    return list(map(int, list(str(num))))
+from functools import reduce
+import operator
 
 
 def persistence(num):
     count_multiplications = 0
-    if num < 10:
-        return count_multiplications
-    
-    list_digits_of_num = make_list_digits_of_num(num)
-    result_num = math.prod(list_digits_of_num)
-    count_multiplications += 1
-
-    while result_num >= 10:
-        list_digits_of_num = make_list_digits_of_num(result_num)
-        result_num = math.prod(list_digits_of_num)
+    while num >= 10:
+        num = reduce(operator.mul, [int(i) for i in str(num)], 1)
         count_multiplications += 1
     return count_multiplications
