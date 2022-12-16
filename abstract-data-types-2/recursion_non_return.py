e8edb56582dce3603919627f63ae52bd0d8004c9
maks-mishin <maks.mishinFZ@gmail.com>
2022-12-17 00:02:46 +0300

    feat: add functions which not return value

---
+++ abstract-data-types-2/recursion_non_return.py
@@ -0,0 +1,18 @@
+def print_even_numbers(input_list, i):
+    """Печать только четных значений списка"""
+    if i == len(input_list):
+        print(flush=True)
+        return
+    if input_list[i] % 2 == 0:
+        print(input_list[i], end=' ')
+    return print_even_numbers(input_list, i + 1)
+
+
+def print_numbers_even_indexes(input_list, i):
+    """Печать элементов списка с четными индексами"""
+    if i == len(input_list):
+        print(flush=True)
+        return
+    if i % 2 == 0:
+        print(input_list[i], end=' ')
+    return print_numbers_even_indexes(input_list, i + 1)
