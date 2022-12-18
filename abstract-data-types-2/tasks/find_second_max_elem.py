f972d99da61606e7e9a2622b200f5aa1f898850d
maks-mishin <maks.mishinFZ@gmail.com>
2022-12-18 21:44:53 +0300

    refactor Find Second Largest: not change input list

--- abstract-data-types-2/tasks/find_second_max_elem.py
+++ abstract-data-types-2/tasks/find_second_max_elem.py
@@ -1,15 +1,16 @@
 def calc_first_second_largest(nums, index, list_largest):
+    result_list = list_largest
     if index < 2:
-        return
+        return list_largest
+
     if nums[index] > list_largest[0]:
-        list_largest[1] = list_largest[0]
-        list_largest[0] = nums[index]
+        result_list = (nums[index], list_largest[0])
     elif nums[index] > list_largest[1] and nums[index] != list_largest[0]:
-        list_largest[1] = nums[index]
-    calc_first_second_largest(nums, index - 1, list_largest)
+        result_list = (list_largest[0], nums[index])
+    return calc_first_second_largest(nums, index - 1, result_list)
 
 
 def find_second_largest_elem(nums):
-    list_largest = [nums[0], nums[1]] if nums[0] > nums[1] else [nums[1], nums[0]]
-    calc_first_second_largest(nums, len(nums) - 1, list_largest)
-    return list_largest[1]
+    init_list = (nums[0], nums[1]) if nums[0] > nums[1] else (nums[1], nums[0])
+    two_largest_elems = calc_first_second_largest(nums, len(nums) - 1, init_list)
+    return two_largest_elems[1]
