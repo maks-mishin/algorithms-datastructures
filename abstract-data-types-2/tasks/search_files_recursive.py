11fa752c7a21ded002f4c5100f048f310a9422b9
maks-mishin <maks.mishinFZ@gmail.com>
2022-12-18 23:44:47 +0300

    add solution Recursive Search In Directory

--- abstract-data-types-2/tasks/search_files_recursive.py
+++ abstract-data-types-2/tasks/search_files_recursive.py
@@ -1,2 +1,20 @@
-def search_files(dir_name):
-    pass
+import os
+
+
+def search_files_in_directory(current_dir_name, list_files=None):
+    if list_files is None:
+        list_files = []
+
+    for dir_name in os.listdir(current_dir_name):
+        abs_path = os.path.join(current_dir_name, dir_name)
+
+        if os.path.isdir(abs_path):
+            search_files_in_directory(abs_path, list_files)
+        if os.path.isfile(abs_path):
+            list_files.append(abs_path)
+    return list_files
+
+
+def search_all_files_in_directory(dir_name):
+    return search_files_in_directory(dir_name)
+
