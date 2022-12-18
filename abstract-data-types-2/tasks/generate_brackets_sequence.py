71ff72050af21736c2a223ae274e4860dda1638c
maks-mishin <maks.mishinFZ@gmail.com>
2022-12-18 23:59:37 +0300

    add solution Generate Parentheses

---
+++ abstract-data-types-2/tasks/generate_brackets_sequence.py
@@ -0,0 +1,15 @@
+def generate_brackets_sequence(p, left, right, parens=None):
+    if parens is None:
+        parens = []
+
+    if left:
+        generate_brackets_sequence(p + '(', left - 1, right, parens)
+    if right > left:
+        generate_brackets_sequence(p + ')', left, right - 1, parens)
+    if not right:
+        parens.append(p)
+    return parens
+
+
+def generate_all_correct_brackets_sequences(count_seq):
+    return generate_brackets_sequence('', count_seq, count_seq)
