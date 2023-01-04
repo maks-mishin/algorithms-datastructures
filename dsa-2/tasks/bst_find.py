b27bc8e9f58ea249b322e47d4236e3430de5d8df
maks-mishin <maks.mishinFZ@gmail.com>
2023-01-04 21:02:27 +0300

    PEP8 for task BST find tree

--- dsa-2/tasks/bst_find.py
+++ dsa-2/tasks/bst_find.py
@@ -1,36 +1,45 @@
+from typing import Optional, Any, Tuple, List
+
+
 class BSTNode:
 
     def __init__(self, key, val, parent):
         self.NodeKey = key
         self.NodeValue = val
         self.Parent = parent
-        self.LeftChild = None
-        self.RightChild = None
+        self.LeftChild: Optional[BSTNode] = None
+        self.RightChild: Optional[BSTNode] = None
 
-    def __str__(self):
+    def __repr__(self):
         return f'Node(key={self.NodeKey})'
+
 
 class BSTFind:
 
     def __init__(self):
-        self.Node = None
+        self.Node: Optional[BSTNode] = None
         self.NodeHasKey = False
         self.ToLeft = False
+
+    def __repr__(self) -> str:
+        return f'BSTFind(Node: {self.Node}, ' \
+               f'HasKey: {self.NodeHasKey}, ' \
+               f'ToLeft: {self.ToLeft})'
 
 
 class BST:
 
-    def __init__(self, node):
+    def __init__(self, node: Optional[BSTNode]) -> None:
         self.Root = node
 
-    def MakeFindResult(self, Node, HasKey, ToLeft):
+    def MakeFindResult(self, Node, HasKey, ToLeft) -> BSTFind:
         FindResult = BSTFind()
         FindResult.Node = Node
         FindResult.NodeHasKey = HasKey
         FindResult.ToLeft = ToLeft
         return FindResult
 
-    def FindNodeByKey(self, key):
+    def FindNodeByKey(self, key) -> BSTFind:
         """
         Ищем в дереве узел и сопутствующую информацию по ключу
         return BSTFind
@@ -52,34 +61,38 @@ class BST:
                 CurrentNode = CurrentNode.RightChild
         return BSTFind()
 
-    def AddKeyValue(self, key, val):
+    def AddKeyValue(self, key: int, val: Any) -> bool:
         """
         Добавляем ключ-значение в дерево.
         return False, если ключ уже есть
         """
-        FindResult = self.FindNodeByKey(key)
-        if FindResult.NodeHasKey:
+        find_result = self.FindNodeByKey(key)
+        if find_result.NodeHasKey:
             return False
         if self.Root is None:
             self.Root = BSTNode(key, val, None)
             return True
-        if FindResult.ToLeft:
-            FindResult.Node.LeftChild = BSTNode(key, val, FindResult.Node)
-        if not FindResult.ToLeft:
-            FindResult.Node.RightChild = BSTNode(key, val, FindResult.Node)
+
+        node_to_insert = BSTNode(key, val, find_result.Node)
+        if find_result.ToLeft:
+            find_result.Node.LeftChild = node_to_insert
+        if not find_result.ToLeft:
+            find_result.Node.RightChild = node_to_insert
         return True
 
-    def FindMaxKey(self, FromNode):
+    def FindMaxKey(self, FromNode: Optional[BSTNode]) -> Optional[BSTNode]:
         while FromNode.RightChild is not None:
             FromNode = FromNode.RightChild
         return FromNode
 
-    def FindMinKey(self, FromNode):
+    def FindMinKey(self, FromNode: Optional[BSTNode]) -> Optional[BSTNode]:
         while FromNode.LeftChild is not None:
             FromNode = FromNode.LeftChild
         return FromNode
 
-    def FinMinMax(self, FromNode, FindMax) -> BSTFind:
+    def FinMinMax(self,
+                  FromNode: Optional[BSTNode],
+                  FindMax: bool) -> Optional[BSTNode]:
         """
         Ищем максимальный/минимальный ключ в поддереве.
         """
@@ -87,7 +100,7 @@ class BST:
             return self.FindMaxKey(FromNode)
         return self.FindMinKey(FromNode)
 
-    def FindSuccessorNode(self, node):
+    def FindSuccessorNode(self, node) -> Optional[BSTNode]:
         if node.RightChild is not None:
             return self.FinMinMax(node.RightChild, False)
         return None
@@ -117,7 +130,7 @@ class BST:
         if NewNode == NewNode.Parent.RightChild:
             NewNode.Parent.RightChild = NewChild
 
-    def DeleteNodeByKey(self, key):
+    def DeleteNodeByKey(self, key: int) -> bool:
         Node = self.FindNodeByKey(key).Node
         if Node.NodeKey != key:
             return False
@@ -130,24 +143,30 @@ class BST:
             Node.NodeValue = NodeSuccessor.NodeValue
         return True
 
-    def CountNodesOfSubtree(self, FromNode, CountNodes):
+    def _count_nodes_of_subtree(self,
+                                FromNode: Optional[BSTNode],
+                                CountNodes: int):
         """Рекурсивный подсчет количества узлов поддерева"""
         CurrentCountNodes = CountNodes
         if FromNode.LeftChild is not None:
-            CurrentCountNodes = self.CountNodesOfSubtree(
+            CurrentCountNodes = self._count_nodes_of_subtree(
                 FromNode.LeftChild, CurrentCountNodes
             )
         if FromNode.RightChild is not None:
-            CurrentCountNodes = self.CountNodesOfSubtree(
+            CurrentCountNodes = self._count_nodes_of_subtree(
                 FromNode.RightChild, CurrentCountNodes
             )
         return CurrentCountNodes + 1
 
     def Count(self):
         """Количество узлов в дереве"""
-        if self.Root is not None:
-            return self.CountNodesOfSubtree(self.Root, 0)
-        return 0
+
+        def _count(node: Optional[BSTNode]):
+            if node is None:
+                return 0
+            return _count(node.LeftChild) + _count(node.RightChild) + 1
+
+        return _count(self.Root)
 
     def WideAllNodes(self):
         if self.Root is None:
@@ -163,29 +182,35 @@ class BST:
 
     def DeepAllNodes(self, OrderType):
         if OrderType == 0:
-            return self.InOrder([], self.Root)
+            return self._get_all_nodes_in_order([], self.Root)
         if OrderType == 1:
-            return self.PostOrder([], self.Root)
+            return self._get_all_nodes_post_order([], self.Root)
         if OrderType == 2:
-            return self.PreOrder([], self.Root)
+            return self._get_all_nodes_pre_order([], self.Root)
 
-    def InOrder(self, ListOfNodes, Node):
-        if Node is not None:
-            self.InOrder(ListOfNodes, Node.LeftChild)
-            ListOfNodes.append(Node)
-            self.InOrder(ListOfNodes, Node.RightChild)
-        return ListOfNodes
+    def _get_all_nodes_in_order(self,
+                                result_list_nodes: List[BSTNode],
+                                node: Optional[BSTNode]):
+        if node is not None:
+            self._get_all_nodes_in_order(result_list_nodes, node.LeftChild)
+            result_list_nodes.append(node)
+            self._get_all_nodes_in_order(result_list_nodes, node.RightChild)
+        return result_list_nodes
 
-    def PostOrder(self, ListOfNodes, Node):
-        if Node is not None:
-            self.PostOrder(ListOfNodes, Node.LeftChild)
-            self.PostOrder(ListOfNodes, Node.RightChild)
-            ListOfNodes.append(Node)
-        return ListOfNodes
+    def _get_all_nodes_post_order(self,
+                                  result_list_nodes: List[BSTNode],
+                                  node: Optional[BSTNode]):
+        if node is not None:
+            self._get_all_nodes_post_order(result_list_nodes, node.LeftChild)
+            self._get_all_nodes_post_order(result_list_nodes, node.RightChild)
+            result_list_nodes.append(node)
+        return result_list_nodes
 
-    def PreOrder(self, ListOfNodes, Node):
-        if Node is not None:
-            ListOfNodes.append(Node)
-            self.PreOrder(ListOfNodes, Node.LeftChild)
-            self.PreOrder(ListOfNodes, Node.RightChild)
-        return ListOfNodes
+    def _get_all_nodes_pre_order(self,
+                                 result_list_nodes: List[BSTNode],
+                                 node: Optional[BSTNode]):
+        if node is not None:
+            result_list_nodes.append(node)
+            self._get_all_nodes_pre_order(result_list_nodes, node.LeftChild)
+            self._get_all_nodes_pre_order(result_list_nodes, node.RightChild)
+        return result_list_nodes
